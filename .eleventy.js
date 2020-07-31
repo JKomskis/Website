432fa32807a49ddab45b9b6362dbfd7470d2fa20
Joseph Komskis <joseph.komskis@gmail.com>
2020-07-30 22:03:04 -0400

    Add resume link Try Azure login

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
         return value.replace('\n', '<br>');
     });
 
+    eleventyConfig.addFilter("getCdnUrl", function (value) {
+        return `${site.cdnUrl}/${value}`;
+    });
+
     return {
         dir: {
             input: "web",
