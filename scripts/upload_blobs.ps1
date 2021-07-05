0b593000542113ed9f8fc96a88b652b70e21a2a2
JKomskis <JKomskis@users.noreply.github.com>
2021-07-05 16:06:37 -0400

    Prevent dependabot from deploying (#51)
    * Prevent dependabot from deploying
    
    * Updated workflows
    
    * Fixed paths
    
    * Finished upload script.

---
+++ scripts/upload_blobs.ps1
@@ -0,0 +1,129 @@
+# Usage: upload_blobs.ps1 <Resource Group Name> <Storage Account Name> <Storage path prefix>
+$rgName=$args[0]
+$acctName=$args[1]
+$blobPrefix=$args[2] ?? ""
+$localFolder="_site"
+
+Set-AzCurrentStorageAccount -ResourceGroupName $rgName -Name $acctName
+
+function Get-LocalBlobs {
+
+    # $blobs = @()
+    Push-Location $localFolder
+    $blobs = (Get-ChildItem -Recurse -File).FullName | Resolve-Path -Relative | ForEach-Object -Parallel {
+        $blob = "" | Select-Object Name,Hash
+        $blob.Name = $_.substring(2) # Remove leading ./
+        $blob.Hash = (Get-FileHash $_ -Algorithm MD5).Hash
+
+        # $blobs += $blob
+        return $blob
+    } -ThrottleLimit 50
+    Pop-Location
+    return $blobs
+}
+
+function Get-RemoteBlobs {
+    param(
+        [Parameter()]
+        [String] $prefix
+    )
+
+    return Get-AzStorageBlob -Container '$web' -Prefix $prefix -ConcurrentTaskCount 50 | ForEach-Object -Parallel {
+        $prefix = $using:prefix
+        $blob = "" | Select-Object Name,Hash
+        $blob.Name = $_.Name
+        $blob.Hash = -join $_.BlobProperties.ContentHash.ForEach('ToString', 'X2')
+
+        if ( ($prefix) -or !($blob.Name -like 'staging/*') ) {
+            return $blob
+        }
+    }
+}
+
+function Set-Blobs {
+    param(
+        [Parameter()]
+        [Object[]] $files
+    )
+
+    $staticAssetCacheControl = "public, max-age=31536000, immutable"
+    $revalidateAssetCacheControl = "no-cache"
+    $properties = @{
+        ".css" = @{
+            CacheControl = $staticAssetCacheControl
+            ContentType = "text/css"
+        }
+        ".js" = @{
+            CacheControl = $staticAssetCacheControl
+            ContentType = "application/javascript"
+        }
+        ".woff2" = @{
+            CacheControl = $staticAssetCacheControl
+            ContentType = "font/woff2"
+        }
+        ".jpg" = @{
+            CacheControl = $staticAssetCacheControl
+            ContentType = "image/jpeg"
+        }
+        ".png" = @{
+            CacheControl = $staticAssetCacheControl
+            ContentType = "image/png"
+        }
+        ".svg" = @{
+            CacheControl = $staticAssetCacheControl
+            ContentType = "image/svg+xml"
+        }
+        ".html" = @{
+            CacheControl = $revalidateAssetCacheControl
+            ContentType = "text/html"
+        }
+    }
+
+    Push-Location $localFolder
+    $files | Foreach-Object -Parallel {
+        $properties_dict = $using:properties
+        Write-Output "Uploading file $_"
+        $path = Get-Item $_
+        $fileProperties = $properties_dict[$path.extension]
+        if($fileProperties) {
+            Set-AzStorageBlobContent -File $path.FullName -Blob $_ -Container '$web' -properties $fileProperties -Force
+        } else {
+            Set-AzStorageBlobContent -File $path.FullName -Blob $_ -Container '$web' -Force
+        }
+    } -ThrottleLimit 50
+    Pop-Location
+}
+
+function Remove-Blobs {
+    param(
+        [Parameter()]
+        [Object[]] $files
+    )
+
+    $files | ForEach-Object -Parallel {
+        Write-Output "Removing file $_"
+        Get-AzStorageBlob -Container '$web' -Blob $_ | Remove-AzStorageBlob
+    } -ThrottleLimit 50
+    
+}
+
+$localFiles = Get-LocalBlobs
+$remoteFiles = (Get-RemoteBlobs $blobPrefix)
+
+$differences = Compare-Object $localFiles $remoteFiles -Property Name, Hash
+
+$missingRemoteFiles = ($differences | Where-Object { $_.SideIndicator -eq "<=" }).Name
+$extraRemoteFiles = ($differences | Where-Object { $_.SideIndicator -eq "=>" }).Name
+
+$extraRemoteFiles = $extraRemoteFiles | Where-Object { $_ -notin $missingRemoteFiles }
+
+if ( $missingRemoteFiles ) {
+    Set-Blobs $missingRemoteFiles
+} else {
+    Write-Output "No blobs to upload"
+}
+if ( $extraRemoteFiles ) {
+    Remove-Blobs $extraRemoteFiles
+} else {
+    Write-Output "No blobs to remove"
+}
