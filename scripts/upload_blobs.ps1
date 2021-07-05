3fc697e25706edd5445397dad8aa388066e02bbb
Joseph Komskis <joseph.komskis@gmail.com>
2021-07-04 20:42:03 -0400

    Updated workflows

---
+++ scripts/upload_blobs.ps1
@@ -0,0 +1,94 @@
+# Usage: upload_blobs.ps1 <Resource Group Name> <Storage Account Name>
+$rgName=$args[0]
+$acctName=$args[1]
+$localFolder="_site"
+
+Set-AzCurrentStorageAccount -ResourceGroupName $rgName -Name $acctName
+
+function Get-LocalBlobs {
+
+    # $blobs = @()
+    Push-Location $localFolder
+    $blobs = (Get-ChildItem -Recurse -File).FullName | Resolve-Path -Relative | ForEach-Object -Parallel {
+        $blob = "" | Select-Object Name,Hash
+        $blob.Name = $_.substring(2) # Remove leading ./
+        $blob.Hash = (Get-FileHash $_ -Algorithm MD5).Hash
+
+        # $blobs += $blob
+        return $blob
+    } -ThrottleLimit 50
+    Pop-Location
+    return $blobs
+}
+
+function Get-ExistingBlobs {
+    param(
+        [Parameter()]
+        [Object[]] $localFiles
+    )
+
+    return Get-AzStorageBlob -Container '$web' -ConcurrentTaskCount 50 | ForEach-Object -Parallel {
+        $blob = "" | Select-Object Name,Hash
+        $blob.Name = $_.Name
+        $blob.Hash = -join $_.BlobProperties.ContentHash.ForEach('ToString', 'X2')
+
+        if ( !($blob.Name -like 'staging/*') ) {
+            return $blob
+        }
+    }
+}
+
+$localFiles = Get-LocalBlobs
+Write-Output "Got Local Files"
+$remoteFiles = Get-ExistingBlobs $localFiles
+Write-Output "Got Remote Files"
+
+Compare-Object $localFiles $remoteFiles
+
+
+# $staticAssetCacheControl = "public, max-age=31536000, immutable"
+# $revalidateAssetCacheControl = "no-cache"
+# $properties = @{
+#     ".css" = @{
+#         CacheControl = $staticAssetCacheControl
+#         ContentType = "text/css"
+#     }
+#     ".js" = @{
+#         CacheControl = $staticAssetCacheControl
+#         ContentType = "application/javascript"
+#     }
+#     ".woff2" = @{
+#         CacheControl = $staticAssetCacheControl
+#         ContentType = "font/woff2"
+#     }
+#     ".jpg" = @{
+#         CacheControl = $staticAssetCacheControl
+#         ContentType = "image/jpeg"
+#     }
+#     ".png" = @{
+#         CacheControl = $staticAssetCacheControl
+#         ContentType = "image/png"
+#     }
+#     ".svg" = @{
+#         CacheControl = $staticAssetCacheControl
+#         ContentType = "image/svg+xml"
+#     }
+#     ".html" = @{
+#         CacheControl = $revalidateAssetCacheControl
+#         ContentType = "text/html"
+#     }
+# }
+
+# cd _site
+# Set-AzCurrentStorageAccount -ResourceGroupName $rgName -Name $acctName
+# Get-ChildItem -File -Recurse |
+# Foreach-Object -Parallel {
+#     $properties_dict = $using:properties
+#     $fileProperties = $properties_dict[$_.extension]
+#     $relativePath = $_ | Resolve-Path -Relative
+#     if($fileProperties) {
+#     Set-AzStorageBlobContent -File $_.fullName -Blob $relativePath -Container '$web' -properties $fileProperties -Force
+#     } else {
+#     Set-AzStorageBlobContent -File $_.fullName -Blob $relativePath -Container '$web' -Force
+#     }
+# } -ThrottleLimit 50
