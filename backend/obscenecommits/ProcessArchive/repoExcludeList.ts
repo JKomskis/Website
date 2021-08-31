d1d27f9e8b2de2a5b856c7ff6ee2d284c73e33ad
Joseph Komskis <joseph.komskis@gmail.com>
2021-08-31 17:16:03 -0400

    Exclude new repo.

--- backend/obscenecommits/ProcessArchive/repoExcludeList.ts
+++ backend/obscenecommits/ProcessArchive/repoExcludeList.ts
@@ -1,1 +1,4 @@
-export const repoExcludeList: Array<string> = ['/enricemcorro/enricemcorro'];
+export const repoExcludeList: Array<string> = [
+    '/enricemcorro/enricemcorro',
+    '/kenneth558/Dysusing-dispensation-no-more',
+];
